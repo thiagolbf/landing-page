6fbbc24d8f69db09b89d50369b2bfd5cd49514d6
Thiago Barcelos <thiagolbf@hotmail.com>
2024-04-13 09:13:10 -0300

    fix: API não pode ser mais utilizada, adicionado os valores no contexto

--- src/providers/products/index.tsx
+++ src/providers/products/index.tsx
@@ -24,16 +24,83 @@ export const ProductsProvider = ({ children }: ProductsProviderProps) => {
   const [products, setProducts] = useState<Product[]>([] as Product[]);
 
   useEffect(() => {
-    axios
-      .get(
-        "https://app.econverse.com.br/teste-front-end/junior/tecnologia/lista-produtos/produtos.json"
-      )
-      .then((res) => {
-        setProducts(res.data.products);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    // axios
+    //   .get(
+    //     "https://app.econverse.com.br/teste-front-end/junior/tecnologia/lista-produtos/produtos.json"
+    //   )
+    //   .then((res) => {
+    //     setProducts(res.data.products);
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //   });
+
+    /*API não pode ser mais utilizada, portanto colocando os dados abaixo para a aplicação
+    voltar a funcionar e conseguir verificar todas as funcionalidades do projetos
+    */
+
+    setProducts([
+      {
+        "productName": "Iphone 11 PRO MAX BRANCO 1",
+        "descriptionShort": "Iphone 11 PRO MAX BRANCO 1",
+        "photo": "https://app.econverse.com.br/teste-front-end/junior/tecnologia/fotos-produtos/foto-iphone.png",
+        "price": 15000
+      },
+      {
+        "productName": "IPHONE 13 MINI 1",
+        "descriptionShort": "IPHONE 13 MINI 1",
+        "photo": "https://app.econverse.com.br/teste-front-end/junior/tecnologia/fotos-produtos/foto-iphone.png",
+        "price": 9000
+      },
+      {
+        "productName": "Iphone 11 PRO MAX BRANCO 2",
+        "descriptionShort": "Iphone 11 PRO MAX BRANCO 2",
+        "photo": "https://app.econverse.com.br/teste-front-end/junior/tecnologia/fotos-produtos/foto-iphone.png",
+        "price": 14990
+      },
+      {
+        "productName": "IPHONE 13 MINI 2",
+        "descriptionShort": "IPHONE 13 MINI 2",
+        "photo": "https://app.econverse.com.br/teste-front-end/junior/tecnologia/fotos-produtos/foto-iphone.png",
+        "price": 12000
+      },
+      {
+        "productName": "Iphone 11 PRO MAX BRANCO 3",
+        "descriptionShort": "Iphone 11 PRO MAX BRANCO 3",
+        "photo": "https://app.econverse.com.br/teste-front-end/junior/tecnologia/fotos-produtos/foto-iphone.png",
+        "price": 4550
+      },
+      {
+        "productName": "IPHONE 13 MINI 3",
+        "descriptionShort": "IPHONE 13 MINI 3",
+        "photo": "https://app.econverse.com.br/teste-front-end/junior/tecnologia/fotos-produtos/foto-iphone.png",
+        "price": 38000
+      },
+      {
+        "productName": "Iphone 11 PRO MAX BRANCO 4",
+        "descriptionShort": "Iphone 11 PRO MAX BRANCO 4",
+        "photo": "https://app.econverse.com.br/teste-front-end/junior/tecnologia/fotos-produtos/foto-iphone.png",
+        "price": 42000
+      },
+      {
+        "productName": "IPHONE 13 MINI 4",
+        "descriptionShort": "IPHONE 13 MINI 4",
+        "photo": "https://app.econverse.com.br/teste-front-end/junior/tecnologia/fotos-produtos/foto-iphone.png",
+        "price": 520
+      },
+      {
+        "productName": "Iphone 11 PRO MAX BRANCO 5",
+        "descriptionShort": "Iphone 11 PRO MAX BRANCO 5",
+        "photo": "https://app.econverse.com.br/teste-front-end/junior/tecnologia/fotos-produtos/foto-iphone.png",
+        "price": 149990
+      },
+      {
+        "productName": "IPHONE 13 MINI 5",
+        "descriptionShort": "IPHONE 13 MINI 5",
+        "photo": "https://app.econverse.com.br/teste-front-end/junior/tecnologia/fotos-produtos/foto-iphone.png",
+        "price": 100000
+      }
+    ])
   }, []);
 
   return (
